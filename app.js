471fecf1cb6c8f75fe4d977047beb6ff96f3ce8c
aatifnehal786 <aatifnehal786@gmail.com>
2025-06-18 22:05:24 +0530

    .

--- app.js
+++ app.js
@@ -603,7 +603,7 @@ app.delete("/delete-chat",auth,async (req,res)=>{
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://wonderful-croquembouche-552960.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }
